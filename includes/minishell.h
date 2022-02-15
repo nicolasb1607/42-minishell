32feb78968cfa8c61b0f2ea171a47cb20f661f96
Nicolas Burat <nburat-d@student.42.fr>
2022-02-15 17:49:22 +0100

    init du repo et debut echo

---
+++ includes/minishell.h
@@ -0,0 +1,8 @@
+#ifndef MINISHELL_H
+
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+# include "echo.h"
+
+#endif
---
+++ main.c
@@ -0,0 +1,6 @@
+#include "minishell.h"
+
+int main(int ac, char **av)
+{
+	ft_echo(ac, av);
+}
