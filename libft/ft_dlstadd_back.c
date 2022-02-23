5b5946e7c14c22ab864865748f9269e27bfe4606
Nicolas Burat <nburat-d@student.42.fr>
2022-02-23 16:23:34 +0100

    Norminette OK

--- libft/ft_dlstadd_back.c
+++ libft/ft_dlstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 17:29:40 by nburat-d          #+#    #+#             */
-/*   Updated: 2022/02/23 14:43:16 by nburat-d         ###   ########.fr       */
+/*   Updated: 2022/02/23 16:23:22 by nburat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	ft_dlstadd_back(t_dlist **alst, t_dlist *new)
 	}
 	if (*alst != NULL && new != NULL)
 	{
-		last = ft_lstlast(*alst);
+		last = ft_dlstlast(*alst);
 		last->next = new;
 		new->prev = last;
 	}	
