a011c3bd36cb6b632762dfd35d6f2966f9f8f6f5
Nicolas_XPS <nicolasburat1607@gmail.com>
2022-02-25 12:00:50 +0100

    ne pas push

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -1,18 +1,2 @@
 #include "export.h"
 
-char **get_var(char *varnvalue)
-{
-	char *varexport;
-
-	varexport = NULL;
-	if (ft_containchar('=', varnvalue) == 0)
-		return (NULL);
-	return (varexport)
-}
-
-
-
-void	ft_export(char *varnvalue, t_dlist **dupenv)
-{
-
-}
