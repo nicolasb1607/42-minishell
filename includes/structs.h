94378e74dd93e65ac6ad53c9e83210fb16eefd08
Nicolas Burat <nburat-d@student.42.fr>
2022-03-01 14:39:18 +0100

    ajout enum quotes struct command update struct token

--- includes/structs.h
+++ includes/structs.h
@@ -3,34 +3,51 @@
 
 # include "minishell.h"
 
-# define T_ARG 1
-# define T_R_DIR 2
-# define T_L_DIR 3
-# define T_DR_DIR 4
-# define T_DL_DIR 5
-# define T_PIPE 6
-# define T_OPERTOR 7
-# define T_STRING 8
-# define T_SPECIAL 9
-# define T_NUMBER 10
-# define T_PUNCTUATION 11 
+# define T_OPERATOR 1 // {'+','-','=','%', '*'}
+# define T_SPECIAL 2// {'$'}
+# define T_NUMBER 3 // {0-9}
+# define T_QUOTE 4 // {'''}
+# define T_DQUOTE 5 // {'"'}
+# define T_PIPE 6 // {'|'}
+# define T_R_DIR 7 // {'>'}
+# define T_L_DIR 8 // {'<'}
+# define T_DR_DIR 9 // {'>>'}
+# define T_DL_DIR 10 // {'<<'}
+
+# define T_STRING 11
+# define T_PUNCTUATION  // {'{','}','(',')', '/', '.'}
+
+typedef enum s_quotes
+{
+	SQUOTES = '\'',
+	DQUOTES = '\"',
+}	t_quotes;
+
+typedef	struct s_command
+{
+	char	*command;
+};
+
+typedef	struct s_lex
+{
+	t_quotes		quotes;
+	t_token			token;
+	struct s_lex	*next;
+	struct s_lex	*prev;
+}					t_lex;
 
 typedef struct s_minishell
 {
+	t_lex	*lexer;
 	t_dlist	*env;
 	t_dlist	**head_env;
 }	t_minishell ;
 
-// typedef struct s_quotes
-// {
-	  
-// };
-
-typedef	struct s_token
+typedef	enum s_token
 {
-	char	*content;
-	int		type;
-	struct s_token	*next;
+	PIPE = '|',
+	R_DIR = '>',
+	L_DIR = '<',
 }			t_token;
 
 
