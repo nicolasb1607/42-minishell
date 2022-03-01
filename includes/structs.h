bc094e62359a0f63d6fed03bc2cc17905a1a3820
Nicolas Burat <nburat-d@student.42.fr>
2022-03-01 11:37:07 +0100

    ajout define pour tokens

--- includes/structs.h
+++ includes/structs.h
@@ -9,6 +9,11 @@
 # define T_DR_DIR 4
 # define T_DL_DIR 5
 # define T_PIPE 6
+# define T_OPERTOR 7
+# define T_STRING 8
+# define T_SPECIAL 9
+# define T_NUMBER 10
+# define T_PUNCTUATION 11 
 
 typedef struct s_minishell
 {
