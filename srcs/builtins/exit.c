0722abf30094cb11b82f195ced441ca6e3749691
Nicolas BURAT DE GURGY <nburat-d@e3r4p10.clusters.42paris.fr>
2022-05-10 14:05:40 +0200

    builtins norme

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 17:37:08 by nburat-d          #+#    #+#             */
-/*   Updated: 2022/05/10 11:29:21 by nburat-d         ###   ########.fr       */
+/*   Updated: 2022/05/10 14:02:19 by nburat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,37 @@
 static int	ft_exit_error_arg(t_tlist **tlst, int *i)
 {
 	if (ft_isdigit((*tlst)->next->token->content[*i]) == 0)
-		{
-			ft_putstr("minishell: exit: numeric argument required\n");
-			return (1);
-		}
-	return(0);
+	{
+		ft_putstr("minishell: exit: numeric argument required\n");
+		return (1);
+	}
+	return (0);
 }
 
-static void exit_number(int error_arg, char *exit_val)
+static void	exit_number(int error_arg, char *exit_val)
 {
 	if (error_arg == 1)
-		exit(2);	
+		exit(2);
 	if (exit_val == NULL)
 		exit(0);
 	else
 		exit(ft_atoi(exit_val) % 256);
 }
 
-
 void	ft_exit(t_tlist *tlst, t_cmd **cmd)
 {
-	char *exit_val;
-	int i;
-	int error_arg;
+	char	*exit_val;
+	int		i;
+	int		error_arg;
 
 	exit_val = NULL;
 	error_arg = 0;
 	i = 0;
-	if(tlst->next)
+	if (tlst->next)
 	{
 		error_arg = ft_exit_error_arg(&tlst, &i);
-		while(tlst->next->token->content[i] && ft_isdigit(tlst->next->token->content[i]) == 1)
+		while (tlst->next->token->content[i]
+			&& ft_isdigit(tlst->next->token->content[i]) == 1)
 		{
 			exit_val = ft_charjoin(exit_val, tlst->next->token->content[i]);
 			i++;
@@ -57,20 +57,14 @@ void	ft_exit(t_tlist *tlst, t_cmd **cmd)
 	free_tlist(&tlst);
 	clear_history();
 	exit_number(error_arg, exit_val);
-	// if (error_arg == 1)
-	// 	exit(2);	
-	// if (exit_val == NULL)
-	// 	exit(0);
-	// else
-	// 	exit(ft_atoi(exit_val) % 256);
 }
 
 void	disp_synt_erro(char *str)
 {
-	printf(RED"minishell: syntax error near unexpected token \'%s\'\n"CRESET, str);
+	printf("minishell: syntax error near unexpected token \'%s\'\n", str);
 }
 
 void	ft_error(char *str)
 {
-	printf(RED"%s\n"CRESET, str);
+	printf(RED "%s\n" CRESET, str);
 }
