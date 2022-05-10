78896ecdd3fa7728a340ab3f39f3229c6ef2efb0
Nathan GOBERT <ngobert@e1r11p17.42.fr>
2022-05-10 14:43:47 +0200

    INIT_FT NORMED

--- srcs/init_ft.c
+++ srcs/init_ft.c
@@ -6,23 +6,25 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 20:14:39 by ngobert           #+#    #+#             */
-/*   Updated: 2022/05/10 12:22:04 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/05/10 14:43:10 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_cmd	*make_commands(t_tlist *tlst, t_cmd **chead, t_dlist **dupenv, t_tlist *curr)
+t_cmd
+	*make_commands(t_tlist *tlst, t_cmd **chead,
+	t_dlist **dupenv, t_tlist *curr)
 {
 	t_cmd	*cmd;
 	char	**path;
-	
+
 	while (tlst)
 	{
 		curr = (tlst);
 		cmd = tlst_to_cmd(&tlst);
 		path = get_path_to_cmd(curr, dupenv);
-		if(update_bin(path, cmd, curr) == -1)
+		if (update_bin(path, cmd, curr) == -1)
 		{
 			path = (ft_free_tab(path), free_tcmd(chead), free_tcmd(&cmd), NULL);
 			if (g_mshell.err_exit != 130)
@@ -37,13 +39,13 @@ t_cmd	*make_commands(t_tlist *tlst, t_cmd **chead, t_dlist **dupenv, t_tlist *cu
 	return (cmd);
 }
 
-void only1cmd(t_tlist *tlst, t_dlist **dupenv, t_cmd *chead)
+void	only1cmd(t_tlist *tlst, t_dlist **dupenv, t_cmd *chead)
 {
-	t_cmd *cmd;
-	pid_t pi;
-	t_tlist *curr;
+	t_cmd	*cmd;
+	pid_t	pi;
+	t_tlist	*curr;
 	t_pipes	pipes;
-	int status;
+	int		status;
 
 	curr = tlst;
 	if (tlst->token->content)
@@ -52,7 +54,7 @@ void only1cmd(t_tlist *tlst, t_dlist **dupenv, t_cmd *chead)
 		if (!cmd)
 			return ;
 		open_io(chead, &pipes);
-		if (ft_strcmp(chead->command,"./minishell") == 0)
+		if (ft_strcmp(chead->command, "./minishell") == 0)
 			signal(SIGINT, SIG_IGN);
 		else
 			signal(SIGINT, handler_cmd);
@@ -120,28 +122,31 @@ t_cmd	*mk_built(t_tlist **tlst)
 	return (cmd);
 }
 
-t_cmd	*mk_cmd_b(t_tlist **tlst, t_dlist **dupenv, t_cmd **chead, t_tlist **curr)
+t_cmd
+	*mk_cmd_b(t_tlist **tlst, t_dlist **dupenv, t_cmd *chead, t_tlist **curr)
 {
-	char	**path;
 	t_cmd	*cmd;
-	t_tlist	*tmp;
-	
-	(void)chead;
-	tmp = (*tlst);
-	cmd = tlst_to_cmd(&tmp);
-	tmp = (*curr);
-	path = get_path_to_cmd(tmp, dupenv);
+	char	**path;
+
+	cmd = tlst_to_cmd(tlst);
+	path = get_path_to_cmd(*curr, dupenv);
+	if (update_bin(path, cmd, *curr) == -1)
+	{
+		path = (ft_free_tab(path), free_tcmd(&chead), free_tcmd(&cmd), NULL);
+		if (g_mshell.err_exit != 130)
+			g_mshell.err_exit = 127;
+		return (NULL);
+	}
 	cmd->is_builtin = (ft_free_tab(path), false);
-	return (dprintf(2, "COUILLE\n"), cmd);
+	return (cmd);
 }
 
 void	init_ft(t_tlist *tlst, t_dlist **dupenv, t_cmd *chead)
 {
-	char	**path;
 	int		nb_cmd;
 	t_cmd	*cmd;
 	t_tlist	*curr;
-	
+
 	nb_cmd = count_command(tlst);
 	if (tlst->token->content)
 	{
@@ -150,18 +155,9 @@ void	init_ft(t_tlist *tlst, t_dlist **dupenv, t_cmd *chead)
 			curr = tlst;
 			if (!is_builtin(curr))
 			{
-				cmd = tlst_to_cmd(&tlst);
-				path = get_path_to_cmd(curr, dupenv);
-				if (update_bin(path, cmd, curr) == -1)
-				{
-					path = (ft_free_tab(path), free_tcmd(&chead), free_tcmd(&cmd), NULL);
-					if (g_mshell.err_exit != 130)
-						g_mshell.err_exit = 127;
+				cmd = mk_cmd_b(&tlst, dupenv, chead, &curr);
+				if (!cmd)
 					return ;
-				}
-				cmd->is_builtin = (ft_free_tab(path), false);
-				dprintf(2, "COUILLE\n");
-				// cmd = mk_cmd_b(&tlst, dupenv, &chead, &curr);
 			}
 			else
 				cmd = mk_built(&tlst);
@@ -170,7 +166,6 @@ void	init_ft(t_tlist *tlst, t_dlist **dupenv, t_cmd *chead)
 			ft_clstadd_back(&chead, cmd);
 		}
 		piping(nb_cmd, &chead, dupenv, tlst);
-		free_tcmd(&chead);
-		free_tlist(&tlst);
+		nb_cmd = (free_tcmd(&chead), free_tlist(&tlst), nb_cmd);
 	}
 }
