223292f64f7e6d4060c98b936dd11fafe6a3a02a
Nicolas Burat <nburat-d@student.42.fr>
2022-05-10 12:58:39 +0200

    push pour l e3

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -3,14 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 17:37:08 by nburat-d          #+#    #+#             */
-/*   Updated: 2022/05/03 22:28:53 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/05/10 11:29:21 by nburat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exit.h"
+
+static int	ft_exit_error_arg(t_tlist **tlst, int *i)
+{
+	if (ft_isdigit((*tlst)->next->token->content[*i]) == 0)
+		{
+			ft_putstr("minishell: exit: numeric argument required\n");
+			return (1);
+		}
+	return(0);
+}
+
+static void exit_number(int error_arg, char *exit_val)
+{
+	if (error_arg == 1)
+		exit(2);	
+	if (exit_val == NULL)
+		exit(0);
+	else
+		exit(ft_atoi(exit_val) % 256);
+}
+
 
 void	ft_exit(t_tlist *tlst, t_cmd **cmd)
 {
@@ -23,28 +44,25 @@ void	ft_exit(t_tlist *tlst, t_cmd **cmd)
 	i = 0;
 	if(tlst->next)
 	{
-		if (ft_isdigit(tlst->next->token->content[i]) == 0)
-		{
-			printf("minishell: exit: %s: numeric argument required\n", tlst->next->token->content);
-			error_arg = 1;
-		}
+		error_arg = ft_exit_error_arg(&tlst, &i);
 		while(tlst->next->token->content[i] && ft_isdigit(tlst->next->token->content[i]) == 1)
 		{
 			exit_val = ft_charjoin(exit_val, tlst->next->token->content[i]);
 			i++;
 		}
 	}
-	printf(GRN"exit\n"CRESET);
+	ft_putstr("exit\n");
 	free_dlist(&g_mshell.env);
 	free_tcmd(cmd);
 	free_tlist(&tlst);
 	clear_history();
-	if (error_arg == 1)
-		exit(2);	
-	if (exit_val == NULL)
-		exit(0);
-	else
-		exit(ft_atoi(exit_val) % 256);
+	exit_number(error_arg, exit_val);
+	// if (error_arg == 1)
+	// 	exit(2);	
+	// if (exit_val == NULL)
+	// 	exit(0);
+	// else
+	// 	exit(ft_atoi(exit_val) % 256);
 }
 
 void	disp_synt_erro(char *str)
