ccdc30a0df89daa46c517bb3afb2598b056a6d08
Nicolas_XPS <nicolasburat1607@gmail.com>
2022-05-11 15:51:34 +0200

    free norme

--- srcs/free.c
+++ srcs/free.c
@@ -3,46 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 14:32:01 by nburat-d          #+#    #+#             */
-/*   Updated: 2022/05/09 11:58:05 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/05/11 15:51:21 by nburat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "free.h"
-
 
 void	free_till_pipe(t_tlist *tlst)
 {
 	int		i;
 	t_tlist	*next;
 	t_tlist	*curr;
-	
-	
+
 	curr = tlst;
 	i = 0;
 	while (curr && curr->prev)
 		curr = (i++, curr->prev);
 	while (curr && ft_strcmp(curr->token->type, T_PIPE) != 0)
 	{
-			next = curr->next;
-			if (curr->token->content)
-			{
-				if (ft_strlen(curr->token->content) != 0)
-					free(curr->token->content);
-				if(curr->token)
-					free(curr->token);
-				free(curr);
-				curr = next;
-			}
+		next = curr->next;
+		if (curr->token->content)
+		{
+			if (ft_strlen(curr->token->content) != 0)
+				free(curr->token->content);
+			if (curr->token)
+				free(curr->token);
+			free(curr);
+			curr = next;
+		}
 	}
 }
 
-void free_tlist(t_tlist **tlst)
+void	free_tlist(t_tlist **tlst)
 {
-	t_tlist *currtok;
-	t_tlist *next;
+	t_tlist	*currtok;
+	t_tlist	*next;
 
 	if (tlst)
 		currtok = *tlst;
@@ -53,11 +51,12 @@ void free_tlist(t_tlist **tlst)
 		if (currtok->token->content)
 		{
 			next = currtok->next;
-			if (currtok->token->content && ft_strlen(currtok->token->content) != 0)
+			if (currtok->token->content
+				&& ft_strlen(currtok->token->content) != 0)
 				free(currtok->token->content);
-			if(currtok->token)
+			if (currtok->token)
 				free(currtok->token);
-			if(currtok)
+			if (currtok)
 				free(currtok);
 			currtok = next;
 		}
